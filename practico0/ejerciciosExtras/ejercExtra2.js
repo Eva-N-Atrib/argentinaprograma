929f9956ffcc902a968ab3fcbc19624ef6499992
Eva-N-Atrib <eva.n.atrib@gmail.com>
2023-06-01 19:19:18 -0300

    se realizó el ejercicio extra 2

---
+++ practico0/ejerciciosExtras/ejercExtra2.js
@@ -0,0 +1,5 @@
+//Ya sabemos, gracias al ejercicio 3, convertir de Fahrenheit a Celsius. ¿Cómo hacemos la conversión de Celsius a Fahrenheit?
+
+let gradosC = 14;
+let gradosF = (gradosC + 32) * (9/5); 
+console.log (`La temperatura en Grados Farenheit es: ${gradosF}`);
