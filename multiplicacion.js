684a2d203cf45f356c47be8d63726e086fc7b8da
Eva-N-Atrib <eva.n.atrib@gmail.com>
2023-05-31 22:36:50 -0300

    "he realizado la multiplicacion.js"

---
+++ multiplicacion.js
@@ -0,0 +1,9 @@
+//Dado tres números calcule la multiplicación entre ellos y lo imprima por pantalla. Deberá guardar este programa en un archivo llamado: multiplicacion.js
+
+//¿Qué valor devuelve su programa si los números son 2 *17 * 6?
+
+let num1 = 2;
+let num2 = 17;
+let num3 = 6;
+let multiplicacion = num1 * num2 * num3;
+console.log(`El valor que devuelve es igual a ${multiplicacion}`);
