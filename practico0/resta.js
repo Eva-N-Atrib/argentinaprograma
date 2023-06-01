a24f2f0ed247e3a53a1ddc559d4db7fc1308b1bb
Eva-N-Atrib <eva.n.atrib@gmail.com>
2023-05-31 22:39:40 -0300

    "he realizado la resta.js"

--- practico0/resta.js
+++ practico0/resta.js
@@ -1,6 +1,6 @@
 //Dado dos números calcule la resta entre ambos; donde el primer número es el minuendo y el segundo es el sustraendo. Imprima el resultado por pantalla. Deberá guardar este programa en un archivo llamado: resta.js.
 
-//¿Qué valor devuelve su programa si el minuendo es 10 y el sustraendo 2?
+//¿Qué valor devuelve su programa si el minuendo es 10 y el sustraendo 2??
 
 let num1 = 10;
 let num2 = 2;
