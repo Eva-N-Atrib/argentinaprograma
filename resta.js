bd08dfe2cbd7379fff245bdfc460088efce1ee41
Eva-N-Atrib <eva.n.atrib@gmail.com>
2023-05-31 22:25:11 -0300

    "realicé la resta"

---
+++ resta.js
@@ -0,0 +1,8 @@
+//Dado dos números calcule la resta entre ambos; donde el primer número es el minuendo y el segundo es el sustraendo. Imprima el resultado por pantalla. Deberá guardar este programa en un archivo llamado: resta.js.
+
+//¿Qué valor devuelve su programa si el minuendo es 10 y el sustraendo 2?
+
+let num1 = 10;
+let num2 = 2;
+let resta = num1 - num2;
+console.log(`El valor que devuelve es igual a ${resta}`);
