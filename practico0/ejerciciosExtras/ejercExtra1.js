505684e250c43c3a46f1d6a45de6619e599ab88c
Eva-N-Atrib <eva.n.atrib@gmail.com>
2023-06-01 19:13:04 -0300

    se sube el 1° ejercicio Extra

---
+++ practico0/ejerciciosExtras/ejercExtra1.js
@@ -0,0 +1,11 @@
+//En el ejercicio 4 se pedía calcular el promedio de 3 números. Supongamos que ahora nos llegan otros tres datos y queremos calcular el promedio de los 6 números. ¿Qué modificaciones debemos hacer al programa?
+
+let n1 = 45;
+let n2 = 66;
+let n3 = 89;
+let n4 = 105;
+let n5 = 13;
+let n6 = 58;
+let suma4 = n1 + n2 + n3 + n4 + n5 + n6;
+let promedio4 = suma4 / 6;
+console.log(`El promedio de los 6 números es: ${promedio4}`);   
