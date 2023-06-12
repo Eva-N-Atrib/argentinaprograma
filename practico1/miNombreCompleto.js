ea56c0629c8fcc855b1d98d1080cfe7751dc754a
Eva-N-Atrib <eva.n.atrib@gmail.com>
2023-06-11 21:04:17 -0300

    se agregó la consigna del punto 1

--- practico1/miNombreCompleto.js
+++ practico1/miNombreCompleto.js
@@ -1,3 +1,16 @@
+// PARTE 1: VARIABLES
+// 1. Escribir un programa que:
+// a. Defina una variable nombre.
+// b. Guarde en la variable nombre tu nombre real.
+// c. Defina y asigne, en un mismo paso, la variable apellido. El valor a
+// asignar deberá ser tu apellido real.
+// d. Repita el paso anterior, pero esta vez definiendo la variable edad y
+// asignandole tu edad real.
+// e. Por último, imprime por consola un mensaje que respete el siguiente
+// formato:
+// "Hola, [nombre] [apellido]. Tienes [edad] años."
+
+
 let nombre;
 nombre = "María Evangelina";
 let apellido = "Nieva Atrib";
