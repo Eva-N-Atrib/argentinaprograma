888c35515aa89b39bff1352c4fe8574ede46d96a
Eva-N-Atrib <eva.n.atrib@gmail.com>
2023-06-11 22:01:53 -0300

    se agrega el archivo de cita.js

--- practico1/miNombreCompleto.js
+++ practico1/miNombreCompleto.js
@@ -2,12 +2,9 @@
 // 1. Escribir un programa que:
 // a. Defina una variable nombre.
 // b. Guarde en la variable nombre tu nombre real.
-// c. Defina y asigne, en un mismo paso, la variable apellido. El valor a
-// asignar deberá ser tu apellido real.
-// d. Repita el paso anterior, pero esta vez definiendo la variable edad y
-// asignandole tu edad real.
-// e. Por último, imprime por consola un mensaje que respete el siguiente
-// formato:
+// c. Defina y asigne, en un mismo paso, la variable apellido. El valor a asignar deberá ser tu apellido real.
+// d. Repita el paso anterior, pero esta vez definiendo la variable edad y asignandole tu edad real.
+// e. Por último, imprime por consola un mensaje que respete el siguiente formato:
 // "Hola, [nombre] [apellido]. Tienes [edad] años."
 
 
