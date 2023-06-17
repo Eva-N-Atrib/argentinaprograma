b06d34e081cbb903d9350cfc56b3f87bd461d393
Eva Nieva Atrib <103968640+Eva-N-Atrib@users.noreply.github.com>
2023-06-16 22:26:05 -0300

    subí nuevamente el archico mayorDeEdad.js

---
+++ practico2/mayorDeEdad.js
@@ -0,0 +1,18 @@
+//a. Solicite al usuario ingresar su edad. Si la edad es mayor o igual a 18, muestra por consola el mensaje: "Eres mayor de edad". De lo contrario, muestra por consola el mensaje: "Eres menor de edad". Guardar en un archivo llamado mayorDeEdad.js
+//Si el número es negativo, muestra por consola el mensaje: "El número ingresado es inválido".
+
+const readlineSync = require('readline-sync');
+const edad = readlineSync.question('A continuación ingresa tu edad: ');
+console.log('Tu edad es %s', edad, 'años');
+    if (edad >= 18)
+    {
+    console.log('Eres mayor de edad');
+    }
+    else if (edad < 0)
+    {
+    console.log('El número ingresado es inválido, intente otra vez ');
+    }
+    else
+    {
+    console.log('Eres menor de edad');
+}
