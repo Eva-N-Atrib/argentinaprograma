699954b249091215ebc25e14691e101e8f4df38f
Eva-N-Atrib <eva.n.atrib@gmail.com>
2023-06-16 22:06:12 -0300

    le incorporé: en el caso de ser negativa la edad, será inválido

--- practico2/mayorDeEdad.js
+++ practico2/mayorDeEdad.js
@@ -1,11 +1,18 @@
 //a. Solicite al usuario ingresar su edad. Si la edad es mayor o igual a 18, muestra por consola el mensaje: "Eres mayor de edad". De lo contrario, muestra por consola el mensaje: "Eres menor de edad". Guardar en un archivo llamado mayorDeEdad.js
+//Si el número es negativo, muestra por consola el mensaje: "El número ingresado es inválido".
 
 const readlineSync = require('readline-sync');
 const edad = readlineSync.question('Ingresa tu edad: ');
 console.log('Tu edad es %s', edad, 'años');
-    if (edad >= 18) {
+    if (edad >= 18)
+    {
     console.log('Eres mayor de edad');
-}
-    else {
+    }
+    else if (edad < 0)
+    {
+    console.log('El número ingresado es inválido');
+    }
+    else
+    {
     console.log('Eres menor de edad');
 }
